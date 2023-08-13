669f1e32bae5285ede4e4ed1a96805c945ed7cf4
hamzabenyazid <hamzabyazid@gmail.com>
2023-08-13 14:37:05 +0100

    add jenkinsfile

---
+++ jenkins/libs/javaRIG/vars/maven.groovy
@@ -0,0 +1,28 @@
+#!/usr/bin/env groovy
+
+def prepareGradleCommand(tasks, props = [:]) {
+    def options = props.collect { k, v ->
+        def option = "-D${k}"
+        if (v != null && !v.toString().isEmpty()) {
+            option += "='${v}'"
+        }
+        return option
+    }.join(' ')
+    return "./mvnw ${tasks} ${options}"
+}
+
+def execute(tasks, props = [:]) {
+    sh prepareGradleCommand(tasks, props)
+}
+
+def executeWithDebug(tasks, props = [:]) {
+    sh prepareGradleCommand(tasks, props) + "--debug"
+}
+
+def executeWithStackTrace(tasks, props = [:]) {
+    sh prepareGradleCommand(tasks, props) + "--exception"
+}
+
+def executeWithDebugStacktrace(tasks, props = [:]) {
+    sh prepareGradleCommand(tasks, props) + "--debug --exception"
+}
